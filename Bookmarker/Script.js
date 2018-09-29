63adb76b0d50a19df18eee6f96980f0d5e5b33e9
Hrishikesh Kamath <kamathhrishi@gmail.com>
2018-09-29 23:57:13 +0530

    Improving :)

--- Bookmarker/Script.js
+++ Bookmarker/Script.js
@@ -7,6 +7,15 @@ function Accept(){
    var WebsiteName=document.getElementById('Web_name').value;
    var WebsiteURL=document.getElementById('Web_URL').value;
 
+   if(!WebsiteName || !WebsiteURL)
+   {
+
+      alert("Please Enter complete Data");
+
+
+   }
+   else{
+
    var bookmark={
 
       Name:WebsiteName,
@@ -31,14 +40,61 @@ function Accept(){
 
    }
 
+ }
+
    e.preventDefault();
+
+}
+
+function DeleteBookmark(Name)
+{
+
+     Saved_Bookmarks.innerHTML="";
+     var bookmarks=JSON.parse(localStorage.getItem('Bookmarks'));
+
+     for(var i=0;i<bookmarks.length;i++)
+     {
+
+
+          if(bookmarks[i].Name==Name)
+          {
+
+
+               bookmarks.splice(i,1);
+
+
+          }
+
+
+     }
+
+     localStorage.setItem('Bookmarks',JSON.stringify(bookmarks));
+
+     FetchBookmarks();
+
 
 }
 
 function FetchBookmarks()
 {
 
-   var bookmarks=localStorage.getItem('Bookmarks');
+   var bookmarks=JSON.parse(localStorage.getItem('Bookmarks'));
+
+   for(var i=0;i<bookmarks.length;i++)
+   {
+          var name=bookmarks[i].Name;
+          var url=bookmarks[i].URL;
+
+          Saved_Bookmarks.innerHTML+='<div class="bookmark_tag">'+
+                                     '<p>'+name+
+                                     '<a href="'+url+'"'+'style="display:inline-block;" class="btn btn-light">Visit</a>'+
+                                     '<a style="display:inline-block;" '+
+                                     'class="btn btn-danger" '+
+                                     'onclick="DeleteBookmark('+"'"+name+"'"+')">'+
+                                     'Delete</a></p>'+
+                                     '</div>'
+
+   }
 
 
 
